d305f7b80dd6ef0d96b0d1c75dc1966285acd716
Juan Dávila <davilajuan926@gmail.com>
2025-08-07 18:16:47 -0500

    Se agrega el archivo 14.py al repo

--- 14.py
+++ 14.py
@@ -1,4 +1,12 @@
 import random
 
 canciones = ["Colombia tierra querida", "Hola", "Tú con él", "Que locura enamorarme de tí"]
-print(f"La canción elegida fue: {random.choice(canciones)}")
+cantidad = int(input("Digite la cantidad de canciones que tendrá su lista: \n"))
+while cantidad > len(canciones):
+    print("La cantidad que pedistes es mayor a la cantidad de canciones que tenemos.")
+    cantidad = int(input("Digite la cantidad de canciones que tendrá su lista: \n"))
+
+lista = random.sample(canciones, cantidad)
+print("La lista de canciones es:")
+for cancion in lista:
+    print(cancion)
