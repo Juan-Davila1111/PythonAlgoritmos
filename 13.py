2052033a45259e3166d9d60dc0d53d20f4fc67a8
Juan Dávila <davilajuan926@gmail.com>
2025-08-07 18:10:34 -0500

    Se agrega el archivo 13.py al repo

---
+++ 13.py
@@ -0,0 +1,8 @@
+import time
+while True:
+    print("Semáforo en Verde 🟢")
+    time.sleep(3)
+    print("Semáforo en amarillo 🟡")
+    time.sleep(2)
+    print("Semáforo en rojo 🔴")
+    time.sleep(3)
